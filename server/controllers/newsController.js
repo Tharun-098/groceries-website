512bef54f5fecb23d2871236060b6d5e92c3d570
Tharun-098 <tharun211388@gmail.com>
2025-06-30 20:35:50 +0530

    email

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,4 +1,5 @@
 import Letter from "../models/letter.js";
+import { sendEmail } from "../utils/nodemailer.js";
 export const subscribeEmail = async (req, res) => {
   const { email } = req.body;
 
@@ -13,6 +14,11 @@ export const subscribeEmail = async (req, res) => {
     }
 
     await Letter.create({ email });
+    await sendEmail(
+      email,
+      "Welcome to Our Newsletter!",
+      "Thanks for subscribing to our newsletter. Stay tuned for updates, deals, and offers!"
+    );
     res.json({ success: true, message: "Subscribed successfully." });
   } catch (err) {
     res.json({ success: false, message: "Server error." });
