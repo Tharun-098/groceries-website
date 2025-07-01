77f3401dfb276118fe052bb1a5bc2bb8fa6209f0
Tharun-098 <tharun211388@gmail.com>
2025-07-01 20:21:40 +0530

    form

--- Client/src/pages/Contact.jsx
+++ Client/src/pages/Contact.jsx
@@ -1,22 +1,35 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { AppContext } from '../context/AppContext';
+import emailjs from 'emailjs-com'
 import toast from 'react-hot-toast';
 const Contact = () => {
     const [name,setName]=useState("")
     const [email,setEmail]=useState("")
     const [message,setMessage]=useState("")
-    const {user,axios,navigate}=useContext(AppContext)
+    const {navigate}=useContext(AppContext)
+    const form=useRef()
     const handleSubmit=async(e)=>{
-        
+            e.preventDefault()
+            emailjs.sendForm("service_jjie8ww", "template_8rrlcra", form.current, {publicKey: "vUmLYHBo3i3DLscL5"})
+      .then(
+        () => {
+          toast.success("Successfully sent")
+          setEmail("")
+          setMessage("")
+          setName("")
+          navigate('/')
+        },
+        (error) => {
+          toast.error('FAILED...', error.text);
+        },
+      );
     }
-     useEffect(()=>{
-     },[])
     return (
     <div className='mt-16 pb-16'>
         <p className='text-2xl md:text-3xl text-gray-500'>Contact Form</p>
         <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
             <div className='flex-1 w-50'>
-                <form className='mt-6 space-y-3 text-sm' onSubmit={handleSubmit}>     
+                <form ref={form} className='mt-6 space-y-3 text-sm' onSubmit={handleSubmit}>     
                     <input type="text" placeholder='Enter the Name' value={name} onChange={(e)=>setName(e.target.value)} name='name' className='w-full px-2 py-3.5 border rounded border-gray-500/30 outline-none text-gray-500 focus-border-primary transition'/>                                  
                     <input type="email" placeholder='Enter the Email' value={email} onChange={(e)=>setEmail(e.target.value)} name='email' className='w-full px-2 py-3.5 border rounded border-gray-500/30 outline-none text-gray-500 focus-border-primary transition'/>
                     <textarea name='message' id="message"  rows="10" value={message} onChange={(e)=>setMessage(e.target.value)} className='w-full px-2 py-3.5 border rounded border-gray-500/30 outline-none text-gray-500 focus-border-primary transition' placeholder='Enter the message'></textarea>
