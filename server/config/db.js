3e035900fd8876e2416768f67d69606f14b949b4
Tharun-098 <tharun211388@gmail.com>
2026-03-16 21:37:51 +0530

    env

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
     try {
         mongoose.connection.on("connected", () => console.log("Database Connected"))
-        const uri = 'mongodb+srv://tharun:tharun123@cluster0.gjhs5os.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-        await mongoose.connect(uri);
+        //const uri = 'mongodb+srv://tharun:tharun123@cluster0.gjhs5os.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+        await mongoose.connect(process.env.URL);
 
     } catch (error) {
         console.log(error.message);
